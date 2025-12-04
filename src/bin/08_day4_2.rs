de6bc24cb6fa7609d3228ca17b7bcb3b3ecc5228
Daniel Ceballos <daniel.ceballos.arvizu@gmail.com>
2025-12-04 10:20:37 -0600

    dia 4

---
+++ src/bin/08_day4_2.rs
@@ -0,0 +1,52 @@
+use std::fs;
+
+fn main() {
+    let input =
+        fs::read_to_string("/home/danny/learning/rust/advent_of_code_2025/input/day4.txt").unwrap();
+
+    let mut grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+
+    let mut result = 0;
+
+    let directions: [(i32, i32); 8] = [
+        (-1, -1),
+        (0, -1),
+        (1, -1),
+        (-1, 0),
+        (1, 0),
+        (-1, 1),
+        (1, 1),
+        (0, 1),
+    ];
+
+    let mut flag = true;
+
+    while flag {
+        flag = false;
+        for i in 0..grid.len() {
+            for j in 0..grid[i].len() {
+                if grid[i][j] == '@' {
+                    let mut aux_counter = 0;
+                    for (xi, yi) in directions {
+                        let ni = (i as i32 + xi) as usize;
+                        let nj = (j as i32 + yi) as usize;
+
+                        if ni < grid.len() && nj < grid[i].len() && grid[ni][nj] == '@' {
+                            aux_counter += 1;
+                        }
+                    }
+
+                    result += if aux_counter < 4 {
+                        grid[i][j] = '.';
+                        flag = true;
+                        1
+                    } else {
+                        0
+                    };
+                }
+            }
+        }
+    }
+
+    println!("result: {}", result);
+}
